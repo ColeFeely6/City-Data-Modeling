a14f30e9f8c4121dc2b457193a4a3c313fb2f0b3
ColeFeely6 <65377055+ColeFeely6@users.noreply.github.com>
2020-10-04 09:57:14 -0400

    Including a count
    working on the load data function

--- project1.py
+++ project1.py
@@ -15,35 +15,39 @@ class COV19Library:
         self.datalist = []
         self.isSorted = False
         self.size = 0
+    def __str__(self):
+        return ("cid "+self.cid)
     def LoadData(self,filename):
         with open(filename,'r') as excel_file:
             sheet_1 = csv.reader(excel_file,delimiter = ',')
-            for row in range(1,len(sheet_1)):
-                cid = row[0]
-                pop = row[2]
-                names = row[1]
-                cstate = []
-                cname = []
-                cities = []
-                for i in range(len(names)):
+            count = 0
+            for row in range(len(sheet_1)):
+                if count != 0:
+                    cid = row[0]
+                    pop = row[2]
+                    names = row[1]
+                    cstate = []
+                    cname = []
+                    cities = []
+                    for i in range(len(names)):
+                        temp = names[i].split(' ')
+                        newtemp = temp[len(temp) - 1]
+                        cstate.append(newtemp)
 
-                    temp = names[i].split(' ')
-                    newtemp = temp[len(temp) - 1]
-                    cstate.append(newtemp)
-
-                    newnewtemp = temp[0:len(temp) - 1]
-                    newstring = ""
-                    for i in range(len(newnewtemp)):
-                        newstring = newstring + newnewtemp[i]
-                        if i != len(newnewtemp):
-                            newstring += " "
-                        cname.append(newstring)
+                        newnewtemp = temp[0:len(temp) - 1]
+                        newstring = ""
+                        for i in range(len(newnewtemp)):
+                            newstring = newstring + newnewtemp[i]
+                            if i != len(newnewtemp):
+                                newstring += " "
+                            cname.append(newstring)
 
                     for i in range(4,65):
                         cities.append(int(row[i]))
 
-                        data = City(cid,cname,cstate,pop,cities)
-                        self.datalist.append(data)
+                    data = City(cid,cname,cstate,pop,cities)
+                    self.datalist.append(data)
+                count += 1
                 self.size = len(self.datalist)
 
 
