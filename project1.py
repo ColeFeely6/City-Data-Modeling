edc6a755dbf4d927200185a7935659d280c27fc7
ColeFeely6 <65377055+ColeFeely6@users.noreply.github.com>
2020-10-04 12:22:13 -0400

    lin search
    update swap the city and attribute clauses
    
    Round 5

--- project1.py
+++ project1.py
@@ -56,9 +56,9 @@ class COV19Library: # Init the class that will manage all the city objects
 
     def linearSearch(self,city,attribute):
         for i in range(len(self.cityArray)):
-            if attribute == self.cityArray[i].cid:
+            if city == self.cityArray[i].cid:
                 return self.cityArray[i]
-            elif city == self.cityArray[i].cname:
+            elif attribute == self.cityArray[i].cname:
                 return self.cityArray[i]
             elif i == len(self.cityArray)-1:
                 return "City not found"
