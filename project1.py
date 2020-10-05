1ac84c6400f61577fb29c3818f0c78209640d8a4
ColeFeely6 <65377055+ColeFeely6@users.noreply.github.com>
2020-10-05 17:41:18 -0400

    Try this, probs need AVL Tree, I have BST

--- project1.py
+++ project1.py
@@ -45,10 +45,68 @@ class TreeNode:
     def hasAnyChildren(self):
         return self.rightChild or self.leftChild
 
-        def hasBothChildren(self):
-            return self.rightChild and self.leftChild
+    def hasBothChildren(self):
+        return self.rightChild and self.leftChild
 
 #######################################################################################################################
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def put(self,key,val):
+        if self.root:
+            self._put(key,val,self.root)
+        else:
+            self.root = TreeNode(key,val)
+        self.size = self.size + 1
+
+    def _put(self,key,val,currentNode):
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                   self._put(key,val,currentNode.leftChild)
+            else:
+                   currentNode.leftChild = TreeNode(key,val,parent=currentNode)
+        else:
+            if currentNode.hasRightChild():
+                   self._put(key,val,currentNode.rightChild)
+            else:
+                   currentNode.rightChild = TreeNode(key,val,parent=currentNode)
+
+    def __setitem__(self,k,v):
+       self.put(k,v)
+
+    def get(self,key):
+       if self.root:
+           res = self._get(key,self.root)
+           if res:
+                  return str(res.payload)
+           else:
+                  return 'City not found'
+       else:
+           return None
+
+    def _get(self,key,currentNode):
+       if not currentNode:
+           return None
+       elif currentNode.key == key:
+           return currentNode
+       elif key < currentNode.key:
+           return self._get(key,currentNode.leftChild)
+       else:
+           return self._get(key,currentNode.rightChild)
+
+    def __getitem__(self,key):
+       return self.get(key)
+
+########################################################################################################################
 
 class COV19Library: # Init the class that will manage all the city objects
     def __init__(self):
@@ -108,9 +166,6 @@ class COV19Library: # Init the class that will manage all the city objects
         else:
             return 'City not found'
 #----------------------------------------------------------------------------------------------------------------------
-
-## TODO I think that this function is creating new names because there should be 942 terms but there are 1884
-    ## TODO check and see if proper swapping
 
     def quickSort(self):
         self.quicksorthelper(0,self.size-1)
@@ -181,16 +236,22 @@ class COV19Library: # Init the class that will manage all the city objects
 
     def buildBST(self):
         #bst = COV19Library()
+        temp = AVLTree()
         for i in range(len(self.cityArray)):
-            self.put(self.cityArray[i].cid, self.cityArray[i].cname)
+            temp.put(self.cityArray[i].cid, self.cityArray[i])
             #bst[self.cityArray[i].cid] = bst[self.cityArray[i].cname]
-    def get(self, key):
+    def searchBST(self,key):
+        return self.get(key)
+
+
+
+    '''def get(self, key):
         if self.root:
             res = self._get(key, self.root)
             if res:
-                return self.root
+                return str(res.payload) ##TODO I think this is returning a tree node and not a City class
             else:
-                return None
+                return 'City not found'
         else:
             return None
 
@@ -205,16 +266,11 @@ class COV19Library: # Init the class that will manage all the city objects
             return self._get(key, currentNode.rightChild)
 
     def searchBST(self,key):
-        return self.get(key)
-
+        return self.get(key)'''
 
 if __name__ == "__main__":
   c = COV19Library()
   c.LoadData('cov19_city.csv')
   #c.buildBST()
-
-  c.quickSort()
-  for i in range(len(c.cityArray)):
-      print(c.cityArray[i].cname,c.cityArray[i].cid,c.cityArray[i].cstate,c.cityArray[i].cities)
 
   #print(c.linearSearch(49780,'id'))
