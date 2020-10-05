2c85ccb6f2a70c4b3e4f77af47db2699fa6e9644
ColeFeely6 <65377055+ColeFeely6@users.noreply.github.com>
2020-10-05 17:47:48 -0400

    Use AVL Balencing

--- project1.py
+++ project1.py
@@ -49,7 +49,7 @@ class TreeNode:
         return self.rightChild and self.leftChild
 
 #######################################################################################################################
-class BinarySearchTree:
+class AVLTree:
 
     def __init__(self):
         self.root = None
@@ -71,14 +71,81 @@ class BinarySearchTree:
     def _put(self,key,val,currentNode):
         if key < currentNode.key:
             if currentNode.hasLeftChild():
-                   self._put(key,val,currentNode.leftChild)
+                self._put(key,val,currentNode.leftChild)
             else:
-                   currentNode.leftChild = TreeNode(key,val,parent=currentNode)
+                currentNode.leftChild = TreeNode(key,val,parent=currentNode)
+                self.updateBalance(currentNode.leftChile)
         else:
             if currentNode.hasRightChild():
-                   self._put(key,val,currentNode.rightChild)
+                self._put(key,val,currentNode.rightChild)
             else:
-                   currentNode.rightChild = TreeNode(key,val,parent=currentNode)
+                currentNode.rightChild = TreeNode(key,val,parent=currentNode)
+                self.updateBalance(currentNode.rightChild)
+
+    def updateBalance(self,node):
+        if node.balanceFactor > 1 or node.balanceFactor < -1:
+            self.rebalance(node)
+            return
+        if node.parent != None:
+            if node.isLeftChild():
+                node.parent.balancefactor += 1
+            elif node.isRightChild():
+                node.parent.balanceFactor -= 1
+
+            if node.parent.balanceFactor != 0:
+                self.updateBalance(node.parent)
+
+    def rotateLeft(self,rotRoot):
+        newRoot = rotRoot.rightChild
+        rotRoot.rightChild = newRoot.leftChild
+        if newRoot.leftChild != None:
+            newRoot.leftChild.parent = rotRoot
+        newRoot.parent = rotRoot.parent
+        if rotRoot.isRoot():
+            self.root = newRoot
+        else:
+            if rotRoot.isLeftChild():
+                rotRoot.parent.leftChild = newRoot
+            else:
+                rotRoot.parent.rightChild = newRoot
+        newRoot.leftChild = rotRoot
+        rotRoot.parent = newRoot
+        rotRoot.balanceFactor = rotRoot.balanceFactor + 1 - min(
+            newRoot.balanceFactor, 0)
+        newRoot.balanceFactor = newRoot.blanceFactor + 1 + max(
+            rotRoot.balanceFactor, 0)
+
+    def rotateRight(self,rotRoot):
+        newRoot = rotRoot.leftChild
+        rotRoot.leftChild = newRoot.rightChild
+        if newRoot.rightChild != None:
+            newRoot.rightChild.parent = rotRoot
+        newRoot.parent = rotRoot.parent
+        if rotRoot.isRoot():
+            self.root = newRoot
+        else:
+            if rotRoot.isRightChild():
+                rotRoot.parent.rightChild = newRoot
+        newRoot.rightChild = rotRoot
+        rotRoot.parent = newRoot
+        rotRoot.balanceFactor = rotRoot.balanceFactor + 1 - min(
+            newRoot.balanceFactor, 0)
+        newRoot.balanceFactor = newRoot.balanceFactor + 1 + max(
+            rotRoot.balanceFactor, 0)
+
+    def rebalance(self,node):
+        if node.balanceFactor < 0:
+            if node.rightChild.balanceFactor > 0:
+                self.rotateRight(node.rightChild)
+                self.rotateLeft(node)
+            else:
+                self.rotateLeft(node)
+        elif node.balanceFactor > 0:
+            if node.leftChild.balanceFactor < 0:
+                self.rotateLeft(node.leftchilc)
+                self.rotateRight(node)
+            else:
+                self.rotateRight(node)
 
     def __setitem__(self,k,v):
        self.put(k,v)
@@ -209,64 +276,16 @@ class COV19Library: # Init the class that will manage all the city objects
 #-----------------------------------------------------------------------------------------------------------------------
 
 
-    def put(self, key, val):
-        if self.root:
-            self._put(key, val, self.root)
-        else:
-            self.root = TreeNode(key, val)
-        self.BSTsize = self.BSTsize + 1
-
-    def _put(self, key, val, currentNode):
-        if key < currentNode.key:
-            if currentNode.hasLeftChild():
-                self._put(key, val, currentNode.leftChild)
-            else:
-                currentNode.leftChild = TreeNode(key, val, parent=currentNode)
-        else:
-            if currentNode.hasRightChild():
-                self._put(key, val, currentNode.rightChild)
-            else:
-                currentNode.rightChild = TreeNode(key, val, parent=currentNode)
-
-    def __setitem__(self, k, v):
-        self.put(k, v)
-
-#-----------------------------------------------------------------------------------------------------------------------
-
-
     def buildBST(self):
         #bst = COV19Library()
         temp = AVLTree()
         for i in range(len(self.cityArray)):
             temp.put(self.cityArray[i].cid, self.cityArray[i])
             #bst[self.cityArray[i].cid] = bst[self.cityArray[i].cname]
+
     def searchBST(self,key):
+        self.buildBST()
         return self.get(key)
-
-
-
-    '''def get(self, key):
-        if self.root:
-            res = self._get(key, self.root)
-            if res:
-                return str(res.payload) ##TODO I think this is returning a tree node and not a City class
-            else:
-                return 'City not found'
-        else:
-            return None
-
-    def _get(self, key, currentNode):
-        if not currentNode:
-            return None
-        elif currentNode.key == key:
-            return currentNode
-        elif key < currentNode.key:
-            return self._get(key, currentNode.leftChild)
-        else:
-            return self._get(key, currentNode.rightChild)
-
-    def searchBST(self,key):
-        return self.get(key)'''
 
 if __name__ == "__main__":
   c = COV19Library()
